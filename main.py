9dbb996afc8af737ef3f85b6b7c2a19072ad6c01
yanniskouyate <yannis.kouyate.com>
2026-05-13 12:19:58 +0200

    correction du fichier main.py le nom de la video

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ model = YOLO("yolo11n.pt")
 # - save=True : crée une vidéo avec les boîtes dessinées
 # - project='runs' : définit le dossier de sortie
 # - name='test_docker' : nom du sous-dossier
-results = model.predict(source="test_video.mp4", save=True, project="runs", name="test_docker", exist_ok=True)
+results = model.predict(source="auto_ecole_test.mov", save=True, project="runs", name="test_docker", exist_ok=True)
 
 print("--- VÉRIFICATION TERMINÉE ---")
 print(f"La vidéo annotée a été enregistrée dans le dossier : runs/test_docker/")
